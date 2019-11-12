ed85619e8a649da9b05cb2840fa43c058bc9ddb4
blitzjakez <blitzjakez@gmail.com>
2019-11-12 17:40:31 -0500

    Adding junk data for testing and updating the calendar creation process to account for special days

--- SortieServer/app.ts
+++ SortieServer/app.ts
@@ -4,6 +4,7 @@ import { Valid16 } from "./classes/validators/Valid16";
 import { Sortie } from "./classes/sortie/Sortie";
 import { Month } from "./classes/calendar/Month";
 import { Week } from "./classes/calendar/Week";
+import { SpecialDays } from "./classes/calendar/SpecialDays";
 
 // Check out https://www.robinwieruch.de/node-express-server-rest-api
 
@@ -53,7 +54,7 @@ app.get('/test', (req, res) => {
 	/* For each month in the  year */
 	for(let month = 0; month < 12; month++)
 	{
-		monthArr[month] = new Month([]); 
+		monthArr[month] = new Month([], new SpecialDays([], [], [])); // Initiate a new month
 		let weekCount:number = 0; // Counter for weeks of this month
 		let dayCount:number = 0; // Counter for the number of days in the week
 		let dayNum:number = (new Date(yearNum, month+1, 0)).getDate(); // Number of days in the month
@@ -78,6 +79,38 @@ app.get('/test', (req, res) => {
 			}
 		}
 	}
+
+	/* Add dummy data for drill, holidays, and training */
+	// Note that you can adjust these two arrays however you want to test your scheduling algorithm
+	var holidays = [new Date(2019, 1, 2), new Date(2019, 5, 5), new Date(2019, 7, 15)]; // These aren't real holiday, just for testing purposes
+	var training = [new Date(2019, 5, 4), new Date(2019, 4, 4), new Date(2019, 2, 10)]; // Again, these are just for training
+	for(var month in monthArr)
+	{
+		monthArr[month].special.drill = monthArr[month].weeks[1].days; // Set the second week of each month to a dirll week
+		for(var week in monthArr[month].weeks)
+		{
+			for(var day in monthArr[month].weeks[week].days)
+			{
+				var dayObj = monthArr[month].weeks[week].days[day]; // Get the day
+				var dateObj = dayObj.dateObj; // Get the date object of the day
+				/* If the date is a holiday */
+				if(holidays.includes(dateObj))
+				{
+					monthArr[month].special.holiday.push(dayObj); // Append it to the holidays array for special
+				}
+
+				/* If the date is a training day */
+				if(training.includes(dateObj))
+				{
+					monthArr[month].special.holiday.push(dayObj); // Append it to the training array for special
+				}
+			}
+		}
+	}
+
+	// Adjust these arrays of flight and numbers how you want for testing your scheduling algorithm
+	var flightAmount = [50, 64, 31, 43, 74, 32, 33, 34, 53, 57, 33, 12]; // Fake number of flights
+	var sortieAmount = [20, 4, 21, 30, 36, 31, 21, 20, 14, 20, 6, 10]; // Fake number of sorties
 
 	return res.send(JSON.stringify(1)); // Send the result (True (1) or False (0)) in the response to the user
 });
